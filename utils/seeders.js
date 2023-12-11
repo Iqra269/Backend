3773f08084dceffc2bc77ee2b764aedfd2d1783b
Iqra269 <269iqra@gmail.com>
2023-12-11 11:38:46 +0500

    Backend completed

--- utils/seeders.js
+++ utils/seeders.js
@@ -1,1 +1,19 @@
-
+ const {models} = require('../models/index');
+ 
+ 
+ 
+ module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Users', [{
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'example@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('Users', null, {});
+    }
+  };
+  
